faac39f9de71bfe09955305d5d5f0a01f893a702
discolarrence <discolarrence@gmail.com>
2022-03-03 22:30:32 -0500

    fixed number generator tests

--- NumberListsTests/NumberListGeneratorTests.cs
+++ NumberListsTests/NumberListGeneratorTests.cs
@@ -25,6 +25,21 @@ namespace DisplayNumberListsTests
         public void ListEvenNumbersStartNumberIsOdd()
         {
             //--Arrange
+            int startNumber = 3;
+            int endNumber = 10;
+            NumberList expected = new NumberList { 4, 6, 8, 10 };
+
+            //--Act
+            NumberList actual = NumberListGenerator.ListEvenNumbers(startNumber, endNumber);
+
+            //--Assert
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void ListOddNumbersStartNumberIsOdd()
+        {
+            //--Arrange
             int startNumber = 5;
             int endNumber = 10;
             NumberList expected = new NumberList { 5, 7, 9 };
@@ -51,20 +66,7 @@ namespace DisplayNumberListsTests
             CollectionAssert.AreEquivalent(expected, actual);
         }
 
-        [TestMethod]
-        public void ListOddNumbersStartNumberIsOdd()
-        {
-            //--Arrange
-            int startNumber = 3;
-            int endNumber = 10;
-            NumberList expected = new NumberList { 4, 6, 8, 10 };
-
-            //--Act
-            NumberList actual = NumberListGenerator.ListEvenNumbers(startNumber, endNumber);
-
-            //--Assert
-            CollectionAssert.AreEquivalent(expected, actual);
-        }
+        
 
     }
 }
