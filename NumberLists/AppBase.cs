e1446a2f830737080dc26950eb0536d136390c67
discolarrence <discolarrence@gmail.com>
2022-03-03 20:44:05 -0500

    app base class

---
+++ NumberLists/AppBase.cs
@@ -0,0 +1,32 @@
+﻿using CodeLouisvilleLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayNumberLists
+{
+    public class AppBase : CodeLouisvilleAppBase
+    {
+        public string AppName { get; set; } = "Number Lists";
+
+        public AppBase(string appName) : base(appName)
+        {
+            appName = AppName;
+        }
+
+        protected override bool Main()
+        {
+            return true;
+        }
+
+        public void StartApp()
+        {
+            Welcome();
+        }
+
+
+
+    }
+}
